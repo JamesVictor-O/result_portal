8cd55a61cf7abe024042d26b0e91406ecb9ab34d
unknown <victorjames40v@gmail.com>
2023-06-05 09:45:18 +0100

    admin being able to upload a single result to a student data base

---
+++ index.js
@@ -0,0 +1,93 @@
+// collectting information from the admin section
+const studentAdmNo = document.querySelector('.studentAdmNo');
+const studentExamYear = document.querySelector('.studentExamYear')
+const studentCourse = document.querySelector('.course')
+const studentScors = document.querySelector('.scors')
+const submitButton = document.querySelector('.btn')
+
+const studentDataBase = [
+    {
+        name: "james victor ochula",
+        addmisionNo: "1310211016",
+        dateOfBirth: "24-05-1995",
+        program: "Diploma in Eletrical Enginneering",
+        batch: " Dec 3. 2020/3rd",
+        courses: {
+            EMS101: "",
+            CED101: "",
+            EPG101: "",
+            TDS101: "",
+            BMS112: "",
+            ESS113: "",
+            EEM112: "",
+            EMS102: "",
+            EIS104: "",
+            PAH104:""
+        }
+    },
+    {
+        name: "musa ahamed baba",
+        addmisionNo: "1310211017",
+        dateOfBirth: "24-05-1995",
+        program: "Diploma in Eletrical Enginneering",
+        batch: " Dec 3. 2020/3rd",
+        courses: {
+            EMS101: "",
+            CED101: "",
+            EPG101: "",
+            TDS101: "",
+            BMS112: "",
+            ESS113: "",
+            EEM112: "",
+            EMS102: "",
+            EIS104: "",
+            PAH104:""
+        }
+    },
+    {
+        name: "musa Ahamad baba",
+        addmisionNo: "1310211018",
+        dateOfBirth: "24-05-1995",
+        program: "Diploma in Eletrical Enginneering",
+        batch: " Dec 3. 2020/3rd",
+        courses: {
+            EMS101: "",
+            CED101: "",
+            EPG101: "",
+            TDS101: "",
+            BMS112: "",
+            ESS113: "",
+            EEM112: "",
+            EMS102: "",
+            EIS104: "",
+            PAH104:""
+        }
+    },
+]
+
+function CollectStudentScorse() {
+    submitButton.addEventListener('click', () => {
+        let studentAdmissionNumber = studentAdmNo.value;
+        let gradingCourse = studentCourse.value;
+        let courseGrade = studentScors.value;
+        
+        // itrating through the student record to search for the person whos is been graded
+        for (let i = 0; i < studentDataBase.length; i++){
+            if (studentDataBase[i].addmisionNo === studentAdmissionNumber) {
+                let courseList=studentDataBase[i].courses
+                for (let i = 0; i < 10; i++){
+                    if (courseList.hasOwnProperty(gradingCourse)) {
+                        courseList[gradingCourse]=courseGrade
+                    } else {
+                        console.log("no")
+                    }
+                }
+                // studentDataBase[i].courses[gradingCourse] = courseGrade;
+                console.log( studentDataBase[i].courses)
+                
+            }
+        }
+        
+    })
+}
+CollectStudentScorse()
